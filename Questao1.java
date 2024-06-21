8dfb232912016b887eef5410d229d1cb7af73451
Carolina Berrafato <85506019+carolinaberrafato@users.noreply.github.com>
2024-06-21 03:05:23 -0300

    Renaming from Main.java to Questao1.java

--- Questao1.java
+++ Questao1.java
@@ -69,13 +69,9 @@ class Membro extends Thread {
     }
 }
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-
+public class Questao1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         Conta conta = new Conta(100000); // Inicializando a conta com R$100.000
 
         // Membros da família que vão movimentar a conta
